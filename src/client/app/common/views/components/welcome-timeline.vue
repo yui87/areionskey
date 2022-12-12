3ca1369915f994b28f60c2d55e855658153cc3ef
atsu1125 <atsuchan@atsuchan.page>
2022-12-12 11:48:10 +0900

    show reactions on welcome timeline
    Welcome TL にリアクションを表示するように

--- src/client/app/common/views/components/welcome-timeline.vue
+++ src/client/app/common/views/components/welcome-timeline.vue
@@ -18,6 +18,7 @@
 				<div class="text">
 					<mfm v-if="note.text" :text="note.cw != null ? note.cw : note.text" :author="note.user" :custom-emojis="note.emojis"/>
 				</div>
+				<mk-reactions-viewer class="reactions" :note="note"/>
 			</div>
 		</div>
 	</transition-group>
@@ -152,5 +153,9 @@ export default Vue.extend({
 
 				> .text
 					text-align left
+					margin 8px
+
+				> .reactions
+					pointer-events none
 
 </style>
