caa864abd7fba89c47547329108d49cd45efe897
fs5m8 <fsco3092@protonmail.com>
2022-12-21 08:52:45 +0900

    Clean: remove hided pages/widget

--- src/client/app/common/views/widgets/hashtags.vue
+++ src/client/app/common/views/widgets/hashtags.vue
@@ -3,8 +3,8 @@
 	<ui-container :show-header="!props.compact">
 		<template #header><fa icon="hashtag"/>{{ $t('title') }}</template>
 
-		<div class="mkw-hashtags--body" :data-mobile="platform == 'mobile'">
-			<mk-trends/>
+		<div class="mkw-hashtags--body">
+			<p class="empty"><fa icon="exclamation-circle"/>this widget has been abolished</p>
 		</div>
 	</ui-container>
 </div>
@@ -20,12 +20,20 @@ export default define({
 		compact: false
 	})
 }).extend({
-	i18n: i18n('common/views/widgets/hashtags.vue'),
-	methods: {
-		func() {
-			this.props.compact = !this.props.compact;
-			this.save();
-		}
-	}
+	i18n: i18n('common/views/widgets/hashtags.vue')
 });
 </script>
+
+<style lang="stylus" scoped>
+.mkw-hashtags
+	.mkw-hashtags--body
+		> .empty
+			margin 0
+			padding 16px
+			text-align center
+			color var(--text)
+
+			> [data-icon]
+				margin-right 4px
+
+</style>
