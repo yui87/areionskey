35cb4baf52b51c72e654c9d51bde7a6011a97a0a
fs5m8 <fsco3092@protonmail.com>
2022-12-27 00:26:23 +0900

    Improve: settings - add isCat/isBot desc

--- src/client/app/common/views/components/settings/profile.vue
+++ src/client/app/common/views/components/settings/profile.vue
@@ -81,8 +81,12 @@
 		<header><fa :icon="faCogs"/> {{ $t('advanced') }}</header>
 
 		<div>
-			<ui-switch v-model="isCat" @change="save(false)">{{ $t('is-cat') }}</ui-switch>
-			<ui-switch v-model="isBot" @change="save(false)">{{ $t('is-bot') }}</ui-switch>
+			<ui-switch v-model="isCat" @change="save(false)">{{ $t('is-cat') }}
+				<template #desc>{{ $t('is-cat-desc') }}</template>
+			</ui-switch>
+			<ui-switch v-model="isBot" @change="save(false)">{{ $t('is-bot') }}
+				<template #desc>{{ $t('is-bot-desc') }}</template>
+			</ui-switch>
 			<ui-switch v-model="alwaysMarkNsfw">{{ $t('@._settings.always-mark-nsfw') }}</ui-switch>
 		</div>
 	</section>
